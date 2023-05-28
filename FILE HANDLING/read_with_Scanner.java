4e2859b7c3643537ce54371229656d2852de8d05
PRASHANT GUPTA <87434010+Prashant844@users.noreply.github.com>
2023-05-28 07:43:55 +0530

    Update read_with_Scanner.java

--- FILE HANDLING/read_with_Scanner.java
+++ FILE HANDLING/read_with_Scanner.java
@@ -5,9 +5,15 @@ public class read3 {
     public static void main(String[] args) {
 
         try {
+            
+            ////here we create the Scanner class object and File Class object together.
+            
             File f = new File("C:/Users/prash/Desktop/New.txt");
+            //and passing the file class object to the Scanner class constructor.
             Scanner UserInput = new Scanner(f);
+            //through the while loop, read line by line
             while (UserInput.hasNextLine()) {
+                //creating a variable input to store the string from the file.
                 String input = UserInput.nextLine();
                 System.out.println(input + " ");
             }
